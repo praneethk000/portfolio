b738f9f5f1d6897f300bb9920b9e6a4c03fa150a
praneethk000 <praneethkpoojary000@gmail.com>
2025-08-21 11:20:25 +0530

    First stages of website

---
+++ app/landingpage/layout.js
@@ -0,0 +1,10 @@
+
+export default function LandingPage() {
+    return (
+        <div>
+            <div className="p-4 w-full bg-blue-300">Header</div>
+            <div>LandingPage</div>
+            <div>Footer</div>
+        </div>
+    )
+}
